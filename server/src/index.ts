eda672337c12e886e3e94b47983a99b670411943
Vaishnavi Mishra <vaishnavimishra986@gmail.com>
2026-06-27 16:48:16 +0530

    feat: add import bulk code

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,5 @@
 export { isValidGSTIN, validateGSTIN } from "./validator";
+export { validateMany } from "./bulk";
 export { parseGSTIN } from "./parser";
 export { getStateByCode, getCodeByState } from "./state-codes";
 export { isValidHSN, isValidSAC } from "./hsn";
